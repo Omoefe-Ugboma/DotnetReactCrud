76c016a16b8091e86f65cbb34aad333b11dae48e
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2025-09-08 15:15:46 +0400

    DotNet Back Api Commit

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,6 +1,19 @@
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// services
+builder.Services.AddControllers();
+
+string connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new ArgumentNullException("connectionString is null");
+builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlite(connectionString));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+// middlewares
+// app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
